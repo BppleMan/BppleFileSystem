231bf77b890002ba0a5ac5f8bdcc0b43324655ed
BppleMan <1286363484@qq.com>
2017-04-28 18:28:59 +0800

    Initial Commit

---
+++ BppleFileSystem/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BppleFileSystem
+//
+//  Created by BppleMan on 17/4/28.
+//  Copyright © 2017年 BppleMan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ BppleFileSystem/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  BppleFileSystem
+//
+//  Created by BppleMan on 17/4/28.
+//  Copyright © 2017年 BppleMan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
